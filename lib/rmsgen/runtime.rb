a0401404735c82d1fcc3f969cf73860923697732
Lake Denman <lake@lakedenman.com>
2011-07-22 03:32:45 -0400

    Refactor

--- lib/rmsgen/runtime.rb
+++ lib/rmsgen/runtime.rb
@@ -18,17 +18,22 @@ module Rmsgen
         note.titleize
         note.inquire
         puts
-        if @output && output = File.open(@output, 'a')
-          output.puts note.to_html
-          output.close
-        else
-          puts note.to_html
-          puts
-        end
+        write(note)
       end
     end
 
     private
+
+    def write(note)
+      output_file = File.open(@output, 'a') if @output
+      if output_file
+        output_file.puts note.to_html
+        output_file.close
+      else
+        puts note.to_html
+        puts
+      end
+    end
 
     def process_notes
       Dir["#{@email_dir}/*"].each do |note|
